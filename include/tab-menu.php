c1256e7f768c566ba1ec2d228ec3ba44c1b3d35d
Shoichi Kawasaki <r02.gm.2100219@da.iwasaki.ac.jp>
2023-02-06 11:05:40 +0900

    タグ修正

--- include/tab-menu.php
+++ include/tab-menu.php
@@ -7,14 +7,18 @@ define('URL', '/OneForAll/マッチングシステム')
     <i class="fa-solid fa-bars"></i>
   </div>
 
+  <?php if ($hrefs[0] !== ''): ?>
   <a href="<?php echo URL?>">
+  <?php endif; ?>
     <div class="menu-btn <?php if ($hrefs[0] === '') echo 'select'; ?>" tab="home">
       <i class="fa-solid fa-house"></i>
       <div class="menu-text">ホーム</div>
     </div>
   </a>
 
+  <?php if ($hrefs[0] !== 'posts'): ?>
   <a href="<?php echo URL?>/posts">
+  <?php endif; ?>
     <div class="menu-btn <?php if ($hrefs[0] === 'posts') echo 'select'; ?>" tab="list">
       <i class="fa-solid fa-table-list"></i>
       <div class="menu-text">
@@ -23,7 +27,9 @@ define('URL', '/OneForAll/マッチングシステム')
     </div>
   </a>
 
+  <?php if ($hrefs[0] !== 'bookmark'): ?>
   <a href="<?php echo URL?>/bookmark">
+  <?php endif; ?>
     <div class="menu-btn <?php if ($hrefs[0] === 'bookmark') echo 'select'; ?>" tab="bookmark">
       <i class="fa-regular fa-bookmark"></i>
       <div class="menu-text">
@@ -32,7 +38,9 @@ define('URL', '/OneForAll/マッチングシステム')
     </div>
   </a>
 
+  <?php if ($hrefs[0] !== 'messages'): ?>
   <a href="<?php echo URL?>/messages">
+  <?php endif; ?>
     <div class="menu-btn <?php if ($hrefs[0] === 'messages') echo 'select'; ?>" tab="messages">
       <i class="fa-regular fa-envelope"></i>
       <div class="menu-text">
@@ -41,7 +49,9 @@ define('URL', '/OneForAll/マッチングシステム')
     </div>
   </a>
 
+  <?php if ($hrefs[0] !== 'my-page'): ?>
   <a href="<?php echo URL?>/my-page">
+  <?php endif; ?>
     <div class="menu-btn <?php if ($hrefs[0] === 'my-page') echo 'select'; ?>" tab="my-page">
       <i class="fa-regular fa-folder"></i>
       <div class="menu-text">
@@ -50,7 +60,9 @@ define('URL', '/OneForAll/マッチングシステム')
     </div>
   </a>
 
+  <?php if ($hrefs[0] !== 'help'): ?>
   <a href="<?php echo URL?>/help">
+  <?php endif; ?>
     <div class="menu-btn <?php if ($hrefs[0] === 'help') echo 'select'; ?>" tab="help">
       <i class="fa-regular fa-circle-question"></i>
       <div class="menu-text">
