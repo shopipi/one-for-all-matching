af943ee81eb9c0662b1949ffb47b3c9b28ed6f4b
Shoichi Kawasaki <r02.gm.2100219@da.iwasaki.ac.jp>
2023-02-06 11:50:34 +0900

    Update 02-bookmark.php

--- pages/02-bookmark.php
+++ pages/02-bookmark.php
@@ -4,9 +4,5 @@ LoadItemWithNoCache('pages/01-posts.css');
 <div class="posts-wrapper">
   <?php
   include 'include/post-card/post-card.php';
-  include 'include/post-card/post-card.php';
-  include 'include/post-card/post-card.php';
-  include 'include/post-card/post-card.php';
-  include 'include/post-card/post-card.php';
   ?>
 </div>
