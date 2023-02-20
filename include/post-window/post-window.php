610944b75f99a2be9861c34eb818578b98a90d21
ryo2021iwasaki <ruchida@iwasaki.ac.jp>
2023-02-20 16:18:53 +0900

    投稿フォーム更新

--- include/post-window/post-window.php
+++ include/post-window/post-window.php
@@ -8,25 +8,27 @@ LoadItemWithNoCache('include/post-window/post-window.js');
 <!-- モーダルエリアここから -->
 <section id="modalArea" class="modalArea">
   <div id="modalBg" class="modalBg"></div>
-  <div class="modalWrapper">
+   <div class="modalWrapper">
     <div class="modalContents">
     <form>
+    <h1>投稿フォーム</h1>
     <p>プロジェクト名：<input type="text"></p>
     <p>プロジェクトの種類：
-    <label><input type="radio" name="radio" value="1" checked>依頼</label>
+    <label><input type="radio" name="radio" value="1">依頼</label>
     <label><input type="radio" name="radio" value="2">宣伝</label>
     </p>
     <p>概要：</p>
     <p><textarea cols="50" rows="5"></textarea></p>
     <p>画像はこちらから！<input type="file" multiple></p>
     <p>マッチング規約に同意する</p>
-    <label><input type="radio" name="radio" value="1" checked>はい</label>
-    <label><input type="radio" name="radio" value="2">いいえ</label>
-    <p><input type="submit" value="送信"></p>
-  </form>
+    <label><input type="checkbox" name="radio" value="1">はい</label>
+    <p style="display: flex; flex-direction: rows; align-items: center;">
+      <input type="submit" value="投稿する">
+    </p>
+    </form>
     </div>
     <div id="closeModal" class="closeModal">
-      ×
+    ×
     </div>
   </div>
 </section>
